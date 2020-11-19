ed161257b9c2baa2572495a675e08bee6d21155c
adrienn timko <adrienn.timko@gmail.com>
2020-11-19 14:39:37 +0100

    Family component: added familyMember nd memberCost, updating sum

--- src/views/Familj.js
+++ src/views/Familj.js
@@ -10,6 +10,7 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
+import { useEffect } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,21 +27,103 @@ const Familj = (props) => {
   //----- if single 3150 else 5680 (in case of partner)
   //---------------------------------------------------
 
-  let familyMembersNumber = 1;
   const [status, setStatus] = useState("single"); //family status
+  const [familyMembers, setFamilyMembers] = useState(1);
+  const [memberCost, setMemberCost] = useState(1010);
   const [kids, setKids] = useState({});
+  const [familyTotal, setFamilyTotal] = useState(0);
 
   const handleChange = (e) => {
     setStatus(e.target.value); //family status: single OR partner
   };
 
-  //------------ function to update the number of kids in different age
-  function kidsHandler(e) {
+  //------------ calculate the number of family members
+  const handelFamilyMembers = () => {
+    let objClone = { ...kids };
+    let kidsNumber = Object.values(objClone).reduce(
+      (prev, current) => parseInt(prev) + parseInt(current),
+      0
+    );
+    if (status === "partner") {
+      setFamilyMembers(kidsNumber + 2);
+    } else {
+      setFamilyMembers(kidsNumber + 1);
+    }
+  };
+
+  //------------ to update the number of kids in different age
+  const kidsHandler = (e) => {
     let kidsData = { ...kids, [e.target.name]: e.target.value };
     setKids(kidsData);
-  }
+    addKidsCost();
+  };
 
-  console.log(status, "familyMembersNumber: ", familyMembersNumber, kids);
+  //------------- to get the costs depending on the number of family members
+  const handelFamilyMemberCost = (num) => {
+    if (num === 1) {
+      setMemberCost(1010);
+    }
+    if (num === 2) {
+      setMemberCost(1120);
+    }
+    if (num === 3) {
+      setMemberCost(1410);
+    }
+    if (num === 4) {
+      setMemberCost(1600);
+    }
+    if (num === 5) {
+      setMemberCost(1850);
+    }
+  };
+
+  //------------ to update the costs according to the number of children in different age
+  //------------ nice to have: reducer hook
+  const addKidsCost = () => {
+    let objClone = { ...kids };
+    let output = Object.entries(objClone).map(([key, value]) => ({
+      [key]: value,
+    })); // make the key-value pairs to an array with objects: [{age1-2: "1"}, {age3: "2"}]
+    console.log(output);
+
+    //map the kidsArray
+    //if (age0-1) {parseInt(age1-2) * 2170} majd add hozzá az összeghez
+    //if (age1-2) {parseInt(age1-2) * 2430} majd add hozzá az összeghez
+  };
+
+  //------------ here to add all the costs to the familyTotal
+  const familyTotalHandler = () => {
+    if (status === "single") {
+      setFamilyTotal(3150 + memberCost); //add costs depending on the number of family members
+    } else {
+      setFamilyTotal(5680 + memberCost); //add costs depending on the number of family members
+    }
+    addKidsCost();
+
+    //add kidsCost
+  };
+
+  useEffect(() => {
+    familyTotalHandler();
+  });
+
+  useEffect(() => {
+    handelFamilyMembers();
+  });
+
+  useEffect(() => {
+    handelFamilyMemberCost(familyMembers);
+  });
+
+  console.log(
+    status,
+    "kids: ",
+    kids,
+    "family members: ",
+    familyMembers,
+    "member cost: ",
+    memberCost
+  );
 
   return (
     <Container>
@@ -111,9 +194,7 @@ const Familj = (props) => {
         {/*to count how many people live in the family */}
         {/*and depending on that number add a sum to familyTotal */}
 
-        <Typography variant="body1">
-          Summa: {status === "partner" ? 5680 : 3150}
-        </Typography>
+        <Typography variant="body1">Summa: {familyTotal}</Typography>
       </form>
     </Container>
   );
