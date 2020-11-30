fe687c8a7ea6ab1fe7409803e9e4da5042fae686
adrienn timko <adrienn.timko@gmail.com>
2020-11-30 15:05:09 +0100

    contect hook with reducer working

--- src/views/Resultat.js
+++ src/views/Resultat.js
@@ -4,21 +4,23 @@ import { Container, Typography } from "@material-ui/core";
 
 const Resultat = () => {
   const { incomeTotal } = useContext(ExpensesContext);
-  const { familyTotal } = useContext(ExpensesContext);
+  const { state } = useContext(ExpensesContext);
   const { expensesTotal } = useContext(ExpensesContext);
 
-  console.log(familyTotal);
+  console.log(state.familyTotal); //undefined
 
   return (
     <Container>
       <Typography variant="h4">Resultat</Typography>
       <Typography variant="body1">Inkomster: {incomeTotal}</Typography>
-      <Typography variant="body1">Familjeförhållande: {familyTotal}</Typography>
+      <Typography variant="body1">
+        Familjeförhållande: {state.familyTotal}
+      </Typography>
       <Typography variant="body1">Utgifter: {expensesTotal}</Typography>
       <br />
       <Typography variant="body1">
         Resultat:{" "}
-        {(incomeTotal || 0) - (familyTotal || 0) - (expensesTotal || 0)}
+        {(incomeTotal || 0) - (state.familyTotal || 0) - (expensesTotal || 0)}
       </Typography>
     </Container>
   );
