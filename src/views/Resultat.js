4a77e6810ab93bbce407763cd543ecaa871d9de2
adrienn timko <adrienn.timko@gmail.com>
2020-12-06 15:06:59 +0100

    added result texts to Resultat

--- src/views/Resultat.js
+++ src/views/Resultat.js
@@ -1,25 +1,41 @@
 import React, { useContext } from "react";
 import { ExpensesContext } from "../context/ExpensesContext";
-import { Container, Typography } from "@material-ui/core";
+import { Box, Container, Typography } from "@material-ui/core";
 
 const Resultat = () => {
   const { incomeTotal } = useContext(ExpensesContext);
   const { state } = useContext(ExpensesContext);
   const { expensesTotal } = useContext(ExpensesContext);
 
+  const result =
+    (incomeTotal || 0) - (state.familyTotal || 0) - (expensesTotal || 0);
+
+  let resultPositivText = `Beräkningen visar att dina månadsinkomster är  ${Math.abs(
+    result
+  )} kr lägre än den nivå som kan ge rätt till försörjningsstöd. Det kan betyda att du får försörjningsstöd om du ansöker. Socialtjänsten gör en individuell bedömning och kan ta hänsyn till annat än ekonomiska faktorer. `;
+  let resultNegativText = `Beräkningen visar att dina månadsinkomster är ${result} kr högre än den nivå som kan ge rätt till försörjningsstöd. Det kan betyda att du inte får försörjningsstöd om du ansöker. Socialtjänsten gör en individuell bedömning och kan ta hänsyn till annat än ekonomiska faktorer. `;
+
   return (
     <Container>
-      <Typography variant="h4">Resultat</Typography>
-      <Typography variant="body1">Inkomster: {incomeTotal}</Typography>
-      <Typography variant="body1">
-        Familjeförhållande: {state.familyTotal}
-      </Typography>
-      <Typography variant="body1">Utgifter: {expensesTotal}</Typography>
-      <br />
-      <Typography variant="body1">
-        Resultat:{" "}
-        {(incomeTotal || 0) - (state.familyTotal || 0) - (expensesTotal || 0)}
-      </Typography>
+      <Box>
+        <Typography variant="h4">Resultat</Typography>
+        <Typography variant="body1">Inkomster: {incomeTotal}</Typography>
+        <Typography variant="body1">
+          Familjeförhållande: {state.familyTotal}
+        </Typography>
+        <Typography variant="body1">Utgifter: {expensesTotal}</Typography>
+        <br />
+        <Typography variant="body1">Resultat: {result}</Typography>
+      </Box>
+      <Box>
+        <Typography variant="body1">
+          {result <= 0 ? resultPositivText : resultNegativText}
+        </Typography>
+        <Typography variant="body1">
+          Kontakta socialtjänsten i din kommun om du vill ansöka om
+          försörjningsstöd eller ha mer information.
+        </Typography>
+      </Box>
     </Container>
   );
 };
